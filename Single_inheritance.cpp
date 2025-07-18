cebb6d626b8e712c113d38860059722abbcf3b50
Wajiha Arif <hackrgirl787@gmail.com>
2025-07-18 19:56:49 +0500

    New commit

--- Single_inheritance.cpp
+++ Single_inheritance.cpp
@@ -1,5 +1,19 @@
 #include<iostream>
 using namespace std;
+
+class Scooty{  // Parent class 
+      
+    int topSpeed; 
+      int mileage; 
+
+}; 
+
+class Bike : Scooty{  // Child class / inherit from scooty 
+   
+     int gear; 
+     
+}; 
+
 int main(){
     cout<< "Hello world " << endl; 
     return 0 ; 
