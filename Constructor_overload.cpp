ffd16f27dee9c4279ac4dde8b997251941e4c1fc
Wajiha Arif <hackrgirl787@gmail.com>
2025-07-20 20:48:04 +0500

    New commit

---
+++ Constructor_overload.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std; 
+
+// same as function overloading __representing the 
+// Polymorphism concept 
+// Like we can make alot of constructor but each 
+// call should go towards a specific constructor that matches with its requirements 
+
+int main(){
+    return 0 ;
+}
